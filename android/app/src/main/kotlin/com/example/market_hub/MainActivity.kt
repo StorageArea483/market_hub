22781ec14fdaa0fb5f9f59b3ff481d6af236c159
Aryan <sajidaryan378@gmail.com>
2026-01-15 11:10:12 +0500

    Stripe Authentication

--- android/app/src/main/kotlin/com/example/market_hub/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/market_hub/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.market_hub
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
